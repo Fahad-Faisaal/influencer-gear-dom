9cb8bb1c0c85d7b3079ace0f3c7b1178c15b15d4
Fahad-Faisaal <fahads1ab@gmail.com>
2022-02-20 21:32:23 +0600

    all button for filtering products added to show all products

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,7 @@ const filterButtons = document.getElementsByClassName('btn-filter');
 for (const button of filterButtons) {
   button.addEventListener('click', function(event) {
     const {catagory} = event.target.dataset;
-    console.log(catagory);
+    
     const products = document.querySelectorAll('.product')
     for (const product of products) {
       if (!product.classList.contains(catagory)) {
@@ -11,6 +11,8 @@ for (const button of filterButtons) {
       } else {
         product.style.display = 'block';
       }
-    }
-  })
-}
+    };
+  });
+};
+
+document.getElementById('search-filter-button').addEventListener('click', function () {})
